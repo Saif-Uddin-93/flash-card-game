1fa5184b835655bfcf01a13f2674b09c0000de11
Saif Uddin <admin@saifuddin.uk>
2023-12-21 21:48:17 +0000

    merge files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,33 +1,65 @@
-//$(()=>{
 // e.g htmlElement("selector")
 const htmlElement = (id) => document.querySelector(id);
 
 // e.g cssClass("#id", "add", "myClass")
 const cssClass = (selector="", action="", cssClass="", element=$(selector)) => {
-    switch (action) {
-      case "add":
-        element.addClass(cssClass);
-        break;
-      case "remove":
-        element.removeClass(cssClass);
-        break;
-      case "toggle":
-        element.toggleClass(cssClass);
-        break;
-      default:
-        break;
-    }
+  switch (action) {
+    case "add":
+      element.addClass(cssClass);
+      break;
+    case "remove":
+      element.removeClass(cssClass);
+      break;
+    case "toggle":
+      element.toggleClass(cssClass);
+      break;
+    default:
+      break;
   }
+}
   
-  // e.g changeText("#id", "lorem ipso")
-  //const changeText = (selector="", txt="", element=$(selector)) => element.text(txt);
-  const changeText = (selector="", txt="", element=htmlElement(selector)) => element.textContent=txt;
+// e.g changeText("#id", "lorem ipso")
+//const changeText = (selector="", txt="", element=$(selector)) => element.text(txt);
+const changeText = (selector="", txt="", element=htmlElement(selector)) => element.textContent=txt;
 
-  // e.g addGlobalEventListener("click", nextQ, "#id")
-  function addGlobalEventListener(typeOfEvent="", callback, selector="", stopPropagation=true) {
-    document.addEventListener(typeOfEvent, (eventObj) => {
-      if (eventObj.target.matches(selector)) callback(eventObj);
-      if (stopPropagation) eventObj.stopPropagation();
-    })
-  }
-//});
+// e.g addGlobalEventListener("click", nextQ, "#id")
+function addGlobalEventListener(typeOfEvent="", callback, selector="", stopPropagation=true) {
+  document.addEventListener(typeOfEvent, (eventObj) => {
+    if (eventObj.target.matches(selector)) callback();
+    if (stopPropagation) eventObj.stopPropagation();
+  })
+}
+
+// Timer object 
+const timeElement = htmlElement("#timer")
+const Timer = {
+  timerInterval: undefined,
+  timeoutInterval: undefined,
+  timeoutSet: (callBack, ms=1000)=> Timer.timeoutInterval = setTimeout(callBack, ms),
+  timeoutClr: ()=> clearTimeout(Timer.timeoutInterval),
+  setActive : (bool = timeElement.dataset.active)=> timeElement.dataset.active = bool.toString(), 
+  active : ()=> String(timeElement.dataset.active),
+  setTime : (time = Timer.getTime())=> timeElement.textContent = time,
+  getTime : ()=> parseInt(timeElement.textContent),
+  start : (ms=1000)=> {Timer.timerInterval = setInterval(Timer.countdown, ms); Timer.setActive(true)},
+  stop : ()=> {clearInterval(Timer.timerInterval); Timer.setActive(false)},
+  deductTime : (time)=> timeElement.textContent=Timer.getTime()-time,
+  countdown: ()=> {Timer.deductTime(1); if(Timer.getTime()<1) Timer.outOfTime();},
+  outOfTime: ()=> {Timer.setTime(0); endScreen();},
+}
+Timer.setTime(100);
+
+// e.g soundVar("path/to/soundFile.wav")
+const soundVar = (src="", audio=document.createElement("audio"), set=audio.setAttribute("src", src)) => audio
+const correct = soundVar("assets/sfx/correct.wav");
+const incorrect = soundVar("assets/sfx/incorrect.wav");
+const soundsLibrary = {
+  sounds: {
+    correct : correct,
+    incorrect : incorrect}, 
+  stop : (s = Object.values(soundsLibrary.sounds)) => s.forEach(sound => {sound.pause(); sound.currentTime = 0;}),
+  play : (stop = soundsLibrary.stop())=> ({
+    correct : ()=> soundsLibrary.sounds.correct.play(),
+    incorrect : ()=> soundsLibrary.sounds.incorrect.play(),}),
+  //newSound: (key="", src="") => soundsLibrary.sounds[key]=soundVar(src),
+}
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,40 +1,53 @@
-//$(()=>{
-const hintEl = $("#hint");
-const questionNoEl = $("#q-number");
-const questAnsEl = $("#question-answer");
-const prevBtnEl = $("#prev-btn");
-const nextBtnEL = $("#next-btn");
 let trackQ = -1;
+// add logic to difficulty() for selecting the difficulty options at start of game
+let difficulty = ()=> "easy"
 
-let diffuculty = "easy"
-
+addGlobalEventListener("click", playBtn, ".play-btn");
 addGlobalEventListener("click", nextBtn, ".next-btn");
-function nextBtn() {
-    trackQ++;
-    trackQ = trackQ===questions[diffuculty].length ? trackQ-1 : trackQ;
-    console.log(trackQ);
-    changeText("#q-number", `${trackQ+1}/${questions[diffuculty].length}`);
-    loadQuestion(diffuculty);
+addGlobalEventListener("click", prevBtn, ".prev-btn");
+addGlobalEventListener("click", loadHint, "#hint");
+
+function playBtn() {
+    /* let timerBool = htmlElement(".play-btn").dataset.timer
+    console.log(timerBool)
+    if (!timerBool){ */
+        Timer.start();
+        //htmlElement(".play-btn").dataset.timer = "true"
+    //}
 }
 
-addGlobalEventListener("click", prevBtn, ".prev-btn");
+function nextBtn() {
+    trackQ++;
+    trackQ = trackQ===questions[difficulty()].length ? trackQ-1 : trackQ;
+    console.log(trackQ);
+    loadQuestion();
+}
+
 function prevBtn() {
     if(trackQ>0)trackQ--;
     if(trackQ<0){
+        console.log("Can't park there, mate!")
         trackQ = -1;
         return;
     }
     console.log(trackQ);
-    changeText("#q-number", `${trackQ+1}/${questions[diffuculty].length}`);
-    loadQuestion(diffuculty);
+    loadQuestion();
 }
 
-function loadQuestion(level){
-    const q=questions[level][trackQ].question;
+function loadQuestion(level=difficulty(), questionNo=trackQ){
+    changeText("#hint", "Hint");
+    changeText("#q-number", `${questionNo+1}/${questions[level].length}`);
+    const q=questions[level][questionNo].question;
     console.log(q);
     changeText("#question-answer", q);
+}
+
+function loadHint(level=difficulty()){
     const h=questions[level][trackQ].hint;
     changeText("#hint", h);
-    //changeText("#question-answer", "TEST")
+    Timer.deductTime(10);
 }
-//});
+
+function endScreen(){
+    console.log("THE END!!")
+}
