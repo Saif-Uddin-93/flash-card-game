0b939f902c331c5008bbd3835708b5e05f4aa37b
Marcin Tatarski <martin.tatarski>
2023-12-26 02:07:18 +0000

    Add fetch request to Pexels API

---
+++ assets/js/words.js
@@ -0,0 +1,88 @@
+const words = [
+    {
+      "animals": [
+        { "word": "Elephant", "hint": "Largest land mammal with long tusks" },
+        { "word": "Tiger", "hint": "Striped big cat known for its strength" },
+        { "word": "Giraffe", "hint": "Tall, long-necked African herbivore" },
+        { "word": "Penguin", "hint": "Flightless bird found in the Southern Hemisphere" },
+        { "word": "Kangaroo", "hint": "Marsupial with powerful hind legs" },
+        { "word": "Dolphin", "hint": "Intelligent marine mammal known for its playful behavior" },
+        { "word": "Lion", "hint": "Fierce carnivorous cat, often called the 'king of the jungle'" },
+        { "word": "Zebra", "hint": "Striped herbivore native to Africa" },
+        { "word": "Panda", "hint": "Bear native to China, known for its black and white fur" },
+        { "word": "Koala", "hint": "Tree-dwelling marsupial native to Australia" },        
+      ]
+    },
+    {     
+      "fruits": [
+        { "word": "Apple", "hint": "A red or green fruit with a crisp texture" },
+        { "word": "Banana", "hint": "A yellow, elongated fruit with a soft edible inner part" },
+        { "word": "Orange", "hint": "A round citrus fruit with a tough bright orange rind" },
+        { "word": "Grapes", "hint": "Small, sweet or sour berries in clusters" },
+        { "word": "Strawberry", "hint": "Red, juicy fruit with tiny seeds on the surface" },
+        { "word": "Kiwi", "hint": "Small, brown, fuzzy fruit with green flesh and black seeds" },
+        { "word": "Pineapple", "hint": "Tropical fruit with a tough, spiky outer layer and sweet inner flesh" },
+        { "word": "Watermelon", "hint": "Large, juicy fruit with green rind and red or pink flesh" },
+        { "word": "Mango", "hint": "Tropical fruit with a sweet and juicy yellow or red flesh" },
+        { "word": "Peach", "hint": "Soft, juicy fruit with a fuzzy skin and a large, hard seed" },        
+      ]
+    },
+    {    
+      "countries": [
+        { "word": "Australia", "hint": "Country known for its unique wildlife, including kangaroos and koalas" },
+        { "word": "Japan", "hint": "Asian country known for its rich cultural heritage and technology" },
+        { "word": "Canada", "hint": "North American country with vast landscapes and maple leaf symbol" },
+        { "word": "Brazil", "hint": "South American country famous for its carnival and Amazon rainforest" },
+        { "word": "France", "hint": "European country known for its art, culture, and cuisine" },
+        { "word": "Germany", "hint": "Central European country with a strong economy and beer culture" },
+        { "word": "Italy", "hint": "Mediterranean country known for its delicious food, art, and history" },
+        { "word": "India", "hint": "South Asian country with diverse cultures, religions, and cuisine" },
+        { "word": "China", "hint": "East Asian country with a rich history, culture, and the Great Wall" },
+        { "word": "Mexico", "hint": "North American country known for its vibrant culture and cuisine" },
+        { "word": "Egypt", "hint": "North African country famous for its ancient pyramids and the Nile River" },        
+      ]
+    },
+    {    
+      "professions": [
+        { "word": "Doctor", "hint": "Medical professional who diagnoses and treats illnesses" },
+        { "word": "Teacher", "hint": "Educator who imparts knowledge and skills to students" },
+        { "word": "Engineer", "hint": "Professional who designs and builds structures, machines, or systems" },
+        { "word": "Pilot", "hint": "Person who operates an aircraft and ensures safe travel" },
+        { "word": "Artist", "hint": "Creative individual who expresses ideas through various forms of art" },
+        { "word": "Chef", "hint": "Culinary expert who prepares and cooks delicious meals" },
+        { "word": "Cleaner", "hint": "Person responsible for maintaining cleanliness and hygiene" },
+        { "word": "Firefighter", "hint": "Heroic individual who extinguishes fires and saves lives" },
+        { "word": "Scientist", "hint": "Professional who conducts research and experiments to discover new knowledge" },
+        { "word": "Journalist", "hint": "Person who gathers, writes, and reports news for the media" },        
+      ]
+    },
+    {
+      "sports": [
+        { "word": "Soccer", "hint": "Team sport with a round ball" },
+        { "word": "Basketball", "hint": "Team sport with a ball and hoop" },
+        { "word": "Tennis", "hint": "Racket sport on a rectangular court" },
+        { "word": "Swimming", "hint": "Activity of moving through water" },
+        { "word": "Cycling", "hint": "Activity of riding bicycles or vehicles" },
+        { "word": "Golf", "hint": "Club-and-ball sport on a course" },
+        { "word": "Volleyball", "hint": "Team sport with a ball over a net" },
+        { "word": "Baseball", "hint": "Bat-and-ball game between two teams" },
+        { "word": "Skiing", "hint": "Winter sport with skis on snow" },
+        { "word": "Cricket", "hint": "Bat-and-ball game between two teams" },        
+      ]
+    }, 
+    {
+      "clothing": [
+        { "word": "Shirt", "hint": "Upper body garment" },
+        { "word": "Pants", "hint": "Lower body garment" },
+        { "word": "Dress", "hint": "Worn for special occasions" },
+        { "word": "Shoes", "hint": "Footwear" },
+        { "word": "Hat", "hint": "Head covering" },
+        { "word": "Jacket", "hint": "Outerwear for upper body" },
+        { "word": "Socks", "hint": "Cover for feet" },
+        { "word": "Gloves", "hint": "Hand coverings" },
+        { "word": "Scarf", "hint": "Neck warmer" },
+        { "word": "Jeans", "hint": "Casual pants" },        
+      ]
+    },    
+  ]
+
