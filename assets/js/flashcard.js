fbb602858714482049696ed607f3f7217c4f1a45
Marcin Tatarski <martin.tatarski>
2023-12-27 01:04:08 +0000

    Fix correct count of guess for new image

--- assets/js/flashcard.js
+++ assets/js/flashcard.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
     guessedLetters = [];
     updateWordDisplay();
     updateScoreDisplay();
+    incorrectGuessCounter = 0;
     // updateMessageDisplay("");
   }
 
