42737d5adef23bd016feeea62a91718418d670e3
Saif Uddin <admin@saifuddin.uk>
2023-12-29 23:06:50 +0000

    fix error with question and answers loading incorrectly

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -76,7 +76,7 @@ function showProgress(bars = trackQ+1){
     // Timer.timeoutSet(loadQuestion, 2);
 } */
 
-function loadQuestion(level=difficulty(), questionNo=trackQ+1){
+function loadQuestion(level=difficulty(), questionNo=trackQ){
     showProgress();
     cssStyle("#footer-msg","visibility","hidden")
     changeText("#q-number", `${questionNo}/${apiResult.results.length/* questions[level].length */}`);
